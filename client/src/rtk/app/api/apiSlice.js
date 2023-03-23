3bf468b90877445b7d22f4812fe9aba1a6957887
Guy T Dev <guytdev@gmail.com>
2023-03-23 13:19:44 +0200

    client/app/apiSlice -> cinema-ws.onrender.com/api

--- client/src/rtk/app/api/apiSlice.js
+++ client/src/rtk/app/api/apiSlice.js
@@ -1,7 +1,7 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
 export const apiSlice = createApi({
-    baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:3000/api' }),
+    baseQuery: fetchBaseQuery({ baseUrl: 'https://cinema-ws.onrender.com/api' }),
     tagTypes: ['User','Movie','Member','Subscription'],
     endpoints: builder => ({})
 })
